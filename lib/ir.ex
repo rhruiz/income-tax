660a17cb114378c1cbb836cde6f546252fec2431
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2017-05-05 21:17:54 -0300

    convert steps into a list to ensure order

--- lib/ir.ex
+++ lib/ir.ex
@@ -1,9 +1,9 @@
 defmodule IR do
-  @callback tax(float(), map(), float()) :: float()
+  @callback tax(float(), [{float(), float()}], float()) :: float()
 
   def tax(salary, method \\ IR.Methods.Recursive) do
     cfg = &Application.get_env(:ir, &1)
 
-    method.tax(salary/1.0, Enum.into(cfg.(:steps), []), cfg.(:max_rate))
+    method.tax(salary/1.0, cfg.(:steps), cfg.(:max_rate))
   end
 end
